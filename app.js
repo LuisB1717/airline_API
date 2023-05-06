a8779c4afb28119d09caf1ed726212a140ec5ba7
LuisB1717 <luisbarboz17@gmail.com>
2023-05-06 11:17:06 -0500

    Database connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const mysql = require("./src/shared/mysql");
 const PORT = 3000;
 
 app.use(express.json());
---
+++ src/shared/mysql.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+async function doQuery(query, params) {
+  const result = await new Promise((resolve, reject) => {
+    pool.query(query, params, function (error, results) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+  return result;
+}
+
+module.exports = { doQuery };
