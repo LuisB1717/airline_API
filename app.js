979e6275fd8195b7fe30c49d535be41fc92cac69
LuisB1717 <luisbarboz17@gmail.com>
2023-06-14 22:05:03 -0500

    Modify file

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const app = express();
-const mysql = require("./src/shared/mysql");
+import express from "express";
+import { buildAirplane } from "./src/flight/airplane-1.js";
+import { doQuery } from "./src/shared/mysql.js";
+
 const PORT = 3000;
+const app = express();
 
 app.use(express.json());
 app.listen(PORT, () => console.log("Server started on port " + PORT));
 
-app.get("/", (req, res) => res.send("hola"));
+app.get("/", async (req, res) => {
+  res.send(await buildAirplane());
+});
+
+app.get("/flights/:id/passengers", async (req, res) => {
+  const result = await doQuery("SELECT * FROM passenger");
+  res.send(result);
+});
